4d599548dd3f3b26ddf45eb4788db6e66a4d1e53
Stéphane Goeuriot <sgoeuriot@comwrap.com>
2017-10-11 17:07:37 +0200

    refactoring the navigation

--- header.php
+++ header.php
@@ -55,25 +55,26 @@
 								<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 
 								<?php // if you'd like to use the site description you can un-comment it below ?>
-								<?php // bloginfo('description'); ?>
+								<?php //bloginfo('description'); ?>
 
 
-								<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-									<?php wp_nav_menu(array(
-										'container' => false,                           // remove nav container
-										'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-										'menu' => __( 'The Main Menu', 'nakedtheme' ),  // nav name
-										'menu_class' => 'nav top-nav cf',               // adding custom nav class
-										'theme_location' => 'main-nav',                 // where it's located in the theme
-										'before' => '',                                 // before the menu
-										'after' => '',                                  // after the menu
-										'link_before' => '',                            // before each link
-										'link_after' => '',                             // after each link
-										'depth' => 0,                                   // limit the depth of the nav
-										'fallback_cb' => ''                             // fallback function (if there is one)
-									)); ?>
+								<nav class="nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+									<?php $menuParameters = array(
+
+										'theme_location'  => 'main-nav',
+    									'container'       => false,
+    									'echo'            => false,
+    									'fallback_cb'     => false,
+    									'items_wrap'      => '%3$s',
+										'before'     => '<div class="nav__item">',
+                    					'after'      => '</div>',
+    									'depth'           => 0
+    								);
+
+									echo strip_tags(wp_nav_menu( $menuParameters ), '<div><a>' );
+									?>
 								</nav>
 
 
 
-						</header>
+					</header>
