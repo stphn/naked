83bb549047e8e67cd14bf7a3760a3271c0103494
Stéphane Goeuriot <sgoeuriot@comwrap.com>
2017-10-12 12:17:07 +0200

    reformated space to tabs

--- post-formats/format-aside.php
+++ post-formats/format-aside.php
@@ -1,53 +1,51 @@
+			<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
 
+				<header class="article-header entry-header">
 
-              <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
+					<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 
-                <header class="article-header entry-header">
+					<p class="byline entry-meta vcard">
+						<?php printf( __( 'Posted', 'nakedtheme' ).' %1$s %2$s',
+						/* the time the post was published */
+						'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+						/* the author of the post */
+						'<span class="by">'.__( 'by', 'nakedtheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+					); ?>
+				</p>
 
-                  <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+			</header> <?php // end article header ?>
 
-                  <p class="byline entry-meta vcard">
-                    <?php printf( __( 'Posted', 'nakedtheme' ).' %1$s %2$s',
-                       /* the time the post was published */
-                       '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       /* the author of the post */
-                       '<span class="by">'.__( 'by', 'nakedtheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    ); ?>
-                  </p>
+			<section class="entry-content cf" itemprop="articleBody">
+				<?php
+				// the content (pretty self explanatory huh)
+				the_content();
 
-                </header> <?php // end article header ?>
+				/*
+				* Link Pages is used in case you have posts that are set to break into
+				* multiple pages. You can remove this if you don't plan on doing that.
+				*
+				* Also, breaking content up into multiple pages is a horrible experience,
+				* so don't do it. While there are SOME edge cases where this is useful, it's
+				* mostly used for people to get more ad views. It's up to you but if you want
+				* to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+				*
+				* http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+				*
+				*/
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'nakedtheme' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+				) );
+				?>
+			</section> <?php // end article section ?>
 
-                <section class="entry-content cf" itemprop="articleBody">
-                  <?php
-                    // the content (pretty self explanatory huh)
-                    the_content();
+			<footer class="article-footer entry-footer">
+				<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'nakedtheme' ) . '</span> ', ', ', '</p>' ); ?>
 
-                    /*
-                     * Link Pages is used in case you have posts that are set to break into
-                     * multiple pages. You can remove this if you don't plan on doing that.
-                     *
-                     * Also, breaking content up into multiple pages is a horrible experience,
-                     * so don't do it. While there are SOME edge cases where this is useful, it's
-                     * mostly used for people to get more ad views. It's up to you but if you want
-                     * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-                     *
-                     * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-                     *
-                    */
-                    wp_link_pages( array(
-                      'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'nakedtheme' ) . '</span>',
-                      'after'       => '</div>',
-                      'link_before' => '<span>',
-                      'link_after'  => '</span>',
-                    ) );
-                  ?>
-                </section> <?php // end article section ?>
+			</footer> <?php // end article footer ?>
 
-                <footer class="article-footer entry-footer">
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'nakedtheme' ) . '</span> ', ', ', '</p>' ); ?>
+			<?php //comments_template(); ?>
 
-                </footer> <?php // end article footer ?>
-
-                <?php //comments_template(); ?>
-
-              </article> <?php // end article ?>
+			</article> <?php // end article ?>
